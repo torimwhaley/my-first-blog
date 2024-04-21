2c51fdbabf7398cb714b693be98ae14cf25cdd9d
Tori Whaley <torimwhaley@abbey.bac.edu>
2024-04-21 14:48:52 +0000

    adding a new view

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,6 @@ from .models import Post
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
