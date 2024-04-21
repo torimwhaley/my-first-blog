8b7cf25271504be5f900067d97bc088f8d9acd5c
Tori Whaley <torimwhaley@abbey.bac.edu>
2024-04-21 14:43:17 +0000

    hot fix

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.utils import timezone
 from .models import Post
 
 # Create your views here.
-def post_list(request, pk):
+def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
