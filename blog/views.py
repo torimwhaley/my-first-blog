9f2f09bb064bc8af64f39428127df2e466cb88db
Tori Whaley <torimwhaley@abbey.bac.edu>
2024-04-21 15:04:48 +0000

    adding to views

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_detail(request, pk):
@@ -12,5 +13,14 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
